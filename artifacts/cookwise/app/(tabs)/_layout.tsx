14b499a77c4f1a184ac26dafd821f40cd9d4c778
ravi223008 <rvij007@aucklanduni.ac.nz>
2026-06-30 16:28:17 +1200

    Update _layout.tsx

--- artifacts/cookwise/app/(tabs)/_layout.tsx
+++ artifacts/cookwise/app/(tabs)/_layout.tsx
@@ -120,7 +120,7 @@ function ClassicTabLayout() {
 }
 
 export default function TabLayout() {
-  if (isLiquidGlassAvailable()) {
+  if (Platform.OS === "ios" && isLiquidGlassAvailable()) {
     return <NativeTabLayout />;
   }
   return <ClassicTabLayout />;
